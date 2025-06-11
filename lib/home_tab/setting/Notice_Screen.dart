5a2e3d0e4386a5007252d2c2355d6aebe6469c87
Mingkong-A <o61257y@naver.com>
2025-06-11 13:45:25 +0900

    앱 시작 아이콘 설정

--- lib/home_tab/setting/Notice_Screen.dart
+++ lib/home_tab/setting/Notice_Screen.dart
@@ -22,7 +22,7 @@ class NoticeScreen extends StatelessWidget {
             '- ATTEA 에 오신걸 환영합니다!\n\n'
                 '- 향후 점검 일정 및 업데이트 사항은 이곳에 공지됩니다.\n\n'
                 '- 건의사항은 settings > 계정 정보에서 전달해주세요.\n\n',
-            style: TextStyle(fontSize: 16),ㅈ
+            style: TextStyle(fontSize: 16),
           ),
         ],
       ),
